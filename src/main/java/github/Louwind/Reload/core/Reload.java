112615b148641b3aeef697677cfb733656f8d2bf
unknown <46799905+Louwind@users.noreply.github.com>
2021-04-06 17:50:39 -0700

    Add: JSON Reload Listener

---
+++ src/main/java/github/Louwind/Reload/core/Reload.java
@@ -0,0 +1,12 @@
+package github.Louwind.Reload.core;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Reload implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+}
