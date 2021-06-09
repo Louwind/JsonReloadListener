6eda341cc56c6ca57540ce21e0947df1cd9722db
Louwind <mr.louwind@gmail.com>
2021-06-08 20:13:47 -0700

    Fix: Using SimpleRegistry::replace

--- src/main/java/github/Louwind/Reload/client/resource/SimpleJsonReloadListener.java
+++ src/main/java/github/Louwind/Reload/client/resource/SimpleJsonReloadListener.java
@@ -2,16 +2,18 @@ package github.Louwind.Reload.client.resource;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.mojang.serialization.Lifecycle;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.BuiltinRegistries;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.SimpleRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Type;
 import java.util.Map;
+import java.util.OptionalInt;
 
 public abstract class SimpleJsonReloadListener<T> extends JsonReloadListener {
 
@@ -19,9 +21,9 @@ public abstract class SimpleJsonReloadListener<T> extends JsonReloadListener {
 
     protected final Gson gson;
     protected final Type type;
-    protected final Registry<T> registry;
+    protected final SimpleRegistry<T> registry;
 
-    public SimpleJsonReloadListener(Gson gson, Type clazz, Registry<T> registry, String dataType) {
+    public SimpleJsonReloadListener(Gson gson, Type clazz, SimpleRegistry<T> registry, String dataType) {
         super(gson, dataType);
 
         this.type = clazz;
@@ -40,11 +42,10 @@ public abstract class SimpleJsonReloadListener<T> extends JsonReloadListener {
                 if(!this.registry.containsId(id))
                     BuiltinRegistries.add(this.registry, id, t);
                 else {
+                    Lifecycle lifecycle = this.registry.getLifecycle();
                     int rawId = this.registry.getRawId(t);
 
-                    this.registry.getKey(t).ifPresent(key -> {
-                        BuiltinRegistries.set(this.registry, rawId, key, t);
-                    });
+                    this.registry.getKey(t).ifPresent(key -> this.registry.replace(OptionalInt.of(rawId), key, t, lifecycle));
                 }
 
             } catch (Exception exception) {
