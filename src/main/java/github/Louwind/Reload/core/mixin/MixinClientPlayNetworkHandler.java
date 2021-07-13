f4c53c64bb05cf9331d646a325e65e221614455d
Louwind <mr.louwind@gmail.com>
2021-07-13 07:40:03 -0700

    Reload 1.3.5 is out!

---
+++ src/main/java/github/Louwind/Reload/core/mixin/MixinClientPlayNetworkHandler.java
@@ -0,0 +1,37 @@
+package github.Louwind.Reload.core.mixin;
+
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static github.Louwind.Reload.core.event.ClientWorldEvents.COLOR_CACHE_LOAD;
+
+@Mixin(ClientPlayNetworkHandler.class)
+public class MixinClientPlayNetworkHandler {
+
+    @Shadow
+    private ClientWorld world;
+
+    @Inject(method = "onPlayerRespawn", at = @At(value = "NEW", target = "net/minecraft/client/world/ClientWorld"))
+    private void onPlayerRespawn(PlayerRespawnS2CPacket packet, CallbackInfo ci) {
+
+        if (this.world != null)
+            COLOR_CACHE_LOAD.invoker().onLoad(((AccessorClientWorld) this.world).getColorCache());
+
+    }
+
+    @Inject(method = "onGameJoin", at = @At(value = "NEW", target = "net/minecraft/client/world/ClientWorld"))
+    private void onGameJoin(GameJoinS2CPacket packet, CallbackInfo ci) {
+
+        if (this.world != null)
+            COLOR_CACHE_LOAD.invoker().onLoad(((AccessorClientWorld) this.world).getColorCache());
+
+    }
+
+}
